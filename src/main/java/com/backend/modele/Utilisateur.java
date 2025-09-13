15ca29e2fafaa3e2dd6ec3b6c9cd3fcb00a3d97a
Samuel <1605602@claurendeau.qc.ca>
2025-09-13 11:13:37 -0400

    Modifications application properties

--- src/main/java/com/backend/modele/Utilisateur.java
+++ src/main/java/com/backend/modele/Utilisateur.java
@@ -1,14 +1,16 @@
 package com.backend.modele;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table
+@Table(name = "utilisateurs")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
 @NoArgsConstructor
 public abstract class Utilisateur {
 
--- src/main/java/com/backend/persistence/EmployeurRepository.java
+++ src/main/java/com/backend/persistence/EmployeurRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeurRepository extends CrudRepository<Employeur, Long> {
 
+    boolean existsByEmail(String email);
+
 }
