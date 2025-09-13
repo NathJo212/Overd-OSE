7e6e11c0c6893aa4f4ee76d79f078a53a4882718
2174720 <vincent8000.lemieux@gmail.com>
2025-09-13 11:15:38 -0400

    Changements minimes à employeur, ajout classe EtudiantService

--- src/main/java/com/backend/modele/Utilisateur.java
+++ src/main/java/com/backend/modele/Utilisateur.java
@@ -1,10 +1,7 @@
 package com.backend.modele;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -13,7 +10,7 @@ import lombok.NoArgsConstructor;
 public abstract class Utilisateur {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
     protected String email;
