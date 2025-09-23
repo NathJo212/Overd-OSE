dddd2660c019bf3bda19ac0e8a6d7b880d894f55
Nathan J <nathanjobin@outlook.com>
2025-09-23 09:45:36 -0400

    Merge de connection à je créer une offre de stage

--- src/main/java/com/backend/service/EmployeurService.java
+++ src/main/java/com/backend/service/EmployeurService.java
@@ -3,7 +3,9 @@ package com.backend.service;
 import com.backend.Exceptions.EmailDejaUtiliseException;
 import com.backend.Exceptions.MotPasseInvalideException;
 import com.backend.modele.Employeur;
+import com.backend.modele.Offre;
 import com.backend.persistence.EmployeurRepository;
+import com.backend.persistence.OffreRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +18,13 @@ public class EmployeurService {
 
     private final PasswordEncoder passwordEncoder;
     private final EmployeurRepository employeurRepository;
+    private final OffreRepository offreRepository;
 
     @Autowired
-    public EmployeurService(PasswordEncoder passwordEncoder, EmployeurRepository employeurRepository) {
+    public EmployeurService(PasswordEncoder passwordEncoder, EmployeurRepository employeurRepository, OffreRepository offreRepository) {
         this.passwordEncoder = passwordEncoder;
         this.employeurRepository = employeurRepository;
+        this.offreRepository = offreRepository;
     }
 
     @Transactional
@@ -36,6 +40,14 @@ public class EmployeurService {
         String hashedPassword = passwordEncoder.encode(password);
         Employeur employeur = new Employeur(email, hashedPassword, telephone, nomEntreprise, contact);
         employeurRepository.save(employeur);
+    }
+
+    @Transactional
+    public void creerOffreDeStage(String titre, String description, String date_debut, String date_fin, String progEtude, String lieuStage, String Remuneration, String dateLimite){
+        //Vérification si user est un employeur (à faire)
+
+        Offre offre = new Offre(titre,description, date_debut, date_fin, progEtude, lieuStage, Remuneration, dateLimite);
+        offreRepository.save(offre);
     }
 
 }
