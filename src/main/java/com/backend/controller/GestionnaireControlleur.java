1ecd2228c4a4a7fd380e866a7e6d86f6083a523e
Abdessalam Benmachiche <1952175@claurendeau.qc.ca>
2025-10-27 18:00:37 -0400

    GestionnaireControlleur.java clean code

--- src/main/java/com/backend/controller/GestionnaireControlleur.java
+++ src/main/java/com/backend/controller/GestionnaireControlleur.java
@@ -3,17 +3,14 @@ package com.backend.controller;
 import com.backend.Exceptions.*;
 import com.backend.service.DTO.*;
 import com.backend.service.GestionnaireService;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
