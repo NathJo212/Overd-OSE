f872327da26bcbb0660b1387beb6a66abef547e9
Nathan J <nathanjobin@outlook.com>
2025-11-04 18:39:07 -0500

    Modif du DTO

--- src/main/java/com/backend/service/DTO/EvaluationDTO.java
+++ src/main/java/com/backend/service/DTO/EvaluationDTO.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,14 +19,16 @@ public class EvaluationDTO {
     private Long etudiantId;
     private Long employeurId;
     private String pdfBase64;
-    private LocalDate dateEvaluation;
+    private LocalDateTime dateEvaluation;
 
     public EvaluationDTO toDTO(Evaluation e) {
         EvaluationDTO dto = new EvaluationDTO();
         dto.setId(e.getId());
         dto.setEntenteId(e.getEntente() != null ? e.getEntente().getId() : null);
         dto.setEtudiantId(e.getEtudiant() != null ? e.getEtudiant().getId() : null);
-        dto.setDateEvaluation(e.getDateEvaluation() != null ? LocalDate.from(e.getDateEvaluation()) : null);
+        dto.setEmployeurId(e.getEmployeur() != null ? e.getEmployeur().getId() : null);
+        dto.setPdfBase64(e.getPdfBase64());
+        dto.setDateEvaluation(e.getDateEvaluation());
         return dto;
     }
 }
