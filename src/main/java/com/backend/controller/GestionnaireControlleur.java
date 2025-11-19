f71da6b5aeff0a9b8e5f7417aa3cb538e514be0b
2174720 <vincent8000.lemieux@gmail.com>
2025-11-19 17:01:25 -0500

    Merge + chatbot + dark theme + yay

--- src/main/java/com/backend/controller/GestionnaireControlleur.java
+++ src/main/java/com/backend/controller/GestionnaireControlleur.java
@@ -14,6 +14,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import com.backend.service.AiService;
 
 import java.util.List;
 
